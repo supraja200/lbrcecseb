3d5171fa6357e87c38b029273933df3da5b07d08
Admin <Admin@DESKTOP-5BD9RGD>
2023-09-08 14:17:35 +0530

    first commit

---
+++ src/com/lbrce/CSE_B/TeamLead.java
@@ -0,0 +1,17 @@
+package com.lbrce.CSE_B;
+
+public class TeamLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TeamLead o=new TeamLead();
+		o.fun();
+		}
+	public static void fun(){
+		System.out.println("This is fun");
+	}
+	
+
+	}
+
+
